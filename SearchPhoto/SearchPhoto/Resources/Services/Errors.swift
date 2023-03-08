15437689a65c811f24ab5bfd4317a112f80afdf1
Slava <Slava7711240@gmail.com>
2023-03-08 21:18:43 +0300

    Add Services and Models

---
+++ SearchPhoto/SearchPhoto/Resources/Services/Errors.swift
@@ -0,0 +1,15 @@
+//
+//  Errors.swift
+//  SearchPhoto
+//
+//  Created by Slava Orlov on 08.03.2023.
+//
+
+import Foundation
+
+enum CustomError: Error {
+    case badUrl
+    case decodeJSON
+    case receivedRequestingData
+    case unknownServerError
+}
---
+++ SearchPhoto/SearchPhoto/Resources/Services/Urls.swift
@@ -0,0 +1,12 @@
+//
+//  Urls.swift
+//  SearchPhoto
+//
+//  Created by Slava Orlov on 08.03.2023.
+//
+
+import Foundation
+
+enum Urls {
+    static let base: String = "https://api.unsplash.com"
+}
